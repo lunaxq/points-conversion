54321b5ab189127075ccd7d26d500f0d84e300d8
zhouxiaoqi <726322625@qq.com>
2024-08-08 16:54:47 +0800

    chore: Introduce tailwindcss

--- .umirc.ts
+++ .umirc.ts
@@ -3,7 +3,9 @@ import { defineConfig } from "umi";
 export default defineConfig({
   routes: [
     { path: "/", component: "index" },
-    { path: "/docs", component: "docs" },
   ],
-  npmClient: 'pnpm',
+
+  npmClient: "pnpm",
+  tailwindcss: {},
+  plugins: ["@umijs/plugins/dist/tailwindcss"],
 });
