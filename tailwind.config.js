8d2ce3c9d85467a9b4a0290b8132d938658933c9
zhouxiaoqi <726322625@qq.com>
2024-08-09 11:49:39 +0800

    chore: Update npmrc and tailwind.config.js
    - Remove unnecessary line from .npmrc
    - Add new color palette to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,4 +4,30 @@ module.exports = {
     './src/components/**/*.tsx',
     './src/layouts/**/*.tsx',
   ],
+  theme: {
+    colors: {
+      primary: '#FC9802',
+      'primary-light': '#FFD5A5',
+      'primary-lighter': '#FFF0D6',
+      'primary-lightest': '#FFF2E9',
+      'primary-dark': '#C57600',
+      green: '#8FCb34',
+      'green-light': '#D6F4A4',
+      'green-dark': '#445D1e',
+      blue: '#E269EC',
+      'blue-light': '#E7F0FF',
+      red: '#FC2F02',
+      'red-light': '#FFEDE9',
+      purple: '#4A51F0',
+      gray: {
+        1: '#333333',
+        2: '#4F4F4F',
+        3: '#828282',
+        4: '#BDBDBD',
+        5: '#E0E0E0',
+        6: '#F4F4F4',
+        7: '#F9F9F9',
+      }
+    }
+  }
 }
