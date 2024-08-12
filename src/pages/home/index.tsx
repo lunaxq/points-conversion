909afb54a00004da15a431f675cf54eae0d9c297
zhouxiaoqi <726322625@qq.com>
2024-08-12 17:40:06 +0800

    feat: basic layout

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -6,6 +6,21 @@ export default function HomePage() {
   const navigate = useNavigate()
 
   return (
-    <Image src={headerImageUrl} onClick={() => navigate('/instructions')} />
+    <div className='h-screen flex flex-col'>
+      <Image src={headerImageUrl} className='flex-none' onClick={() => navigate('/instructions')} />
+      <div className='flex-1 overflow-hidden flex'>
+        <div className='w-[92px] flex-none h-full'>
+          {/* TODO: SideBar */}
+        </div>
+        <div className='flex-1 h-full flex flex-col overflow-hidden'>
+          <div className='flex-none'>
+            {/* TODO: TabsFilter */}
+          </div>
+          <div className='flex-1 h-full overflow-y-scroll'>
+            {/* TODO: MainContent */}
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
